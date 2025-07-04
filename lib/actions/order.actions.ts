ed5482c699adeed869a6e37e650d6edc692c4b73
taemin <tpark01@hotmail.com>
2025-07-04 11:36:00 +0900

    section 7

---
+++ lib/actions/order.actions.ts
@@ -0,0 +1,116 @@
+'use server';
+
+import { isRedirectError } from 'next/dist/client/components/redirect-error';
+import { convertToPlainObject, formatError } from '../utils';
+import { auth } from '@/auth';
+import { getMyCart } from './cart.actions';
+import { getUserById } from './user.actions';
+import { insertOrderSchema } from '../validators';
+import { prisma } from '@/db/prisma';
+import { CartItem } from '@/types';
+
+// create order and create the order items
+export async function createOrder() {
+  try {
+    const session = await auth();
+
+    if (!session) throw new Error('User is not authenticated');
+
+    const cart = await getMyCart();
+    const userId = session?.user?.id;
+
+    if (!userId) throw new Error('User not found');
+
+    const user = await getUserById(userId);
+
+    if (!cart || cart.items.length == 0) {
+      return {
+        success: false,
+        message: 'Your cart is empty',
+        redirectTo: '/cart',
+      };
+    }
+
+    if (!user.address) {
+      return {
+        success: false,
+        message: 'No shipping address',
+        redirectTo: '/shipping-address',
+      };
+    }
+
+    if (!user.paymentMethod) {
+      return {
+        success: false,
+        message: 'No payment method',
+        redirectTo: '/payment-method',
+      };
+    }
+
+    // create order object
+    const order = insertOrderSchema.parse({
+      userId: user.id,
+      shippingAddress: user.address,
+      paymentMethod: user.paymentMethod,
+      itemsPrice: cart.itemsPrice,
+      shippingPrice: cart.shippingPrice,
+      taxPrice: cart.taxPrice,
+      totalPrice: cart.totalPrice,
+    });
+
+    // create a transaction to create order and order items in database
+    const insertedOrderId = await prisma.$transaction(async (tx) => {
+      // create order
+      const insertedOrder = await tx.order.create({ data: order });
+      // create order items from the cart items
+      for (const item of cart.items as CartItem[]) {
+        await tx.orderItem.create({
+          data: {
+            ...item,
+            price: item.price,
+            orderId: insertedOrder.id,
+          },
+        });
+      }
+
+      // clear cart
+      await tx.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: [],
+          totalPrice: 0,
+          taxPrice: 0,
+          shippingPrice: 0,
+          itemsPrice: 0,
+        },
+      });
+
+      return insertedOrder.id;
+    });
+
+    if (!insertedOrderId) {
+      throw new Error('Order not created');
+    }
+    return {
+      success: true,
+      message: 'Order created',
+      redirectTo: `/order/${insertedOrderId}`,
+    };
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// get order by id
+export async function getOrderById(orderId: string) {
+  const data = await prisma.order.findFirst({
+    where: { id: orderId },
+    include: {
+      orderitems: true,
+      user: { select: { name: true, email: true } },
+    },
+  });
+
+  return convertToPlainObject(data);
+}
--- lib/validators.ts
+++ lib/validators.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { formatNumberWithDecimal } from './utils';
+import { PAYMENT_METHODS } from './constants';
 
 const currency = z
   .string()
@@ -76,4 +77,37 @@ export const shippingAddressSchema = z.object({
   country: z.string().min(3, 'Country must be at least 3 characters'),
   lat: z.number().optional(),
   lng: z.number().optional(),
+});
+
+// schema for payment method
+export const paymentMethodSchema = z
+  .object({
+    type: z.string().min(1, 'Payment Method is required'),
+  })
+  .refine((data) => PAYMENT_METHODS.includes(data.type), {
+    path: ['type'],
+    message: 'Invalid payment method',
+  });
+
+// schema for inserting order
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, 'User is required'),
+  itemsPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  totalPrice: currency,
+  paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+    message: 'Ivalid payment method',
+  }),
+  shippingAddress: shippingAddressSchema,
+});
+
+// schema for inserting order item
+export const insertOrderItemSchema = z.object({
+  productId: z.string(),
+  slug: z.string(),
+  image: z.string(),
+  name: z.string(),
+  price: currency,
+  qty: z.number(),
 });
