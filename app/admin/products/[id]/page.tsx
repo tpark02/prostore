2e34332476e4ce664cfc2fa8550915b32efc6354
taemin <tpark01@hotmail.com>
2025-07-07 14:54:06 +0900

    error fix

--- app/admin/products/[id]/page.tsx
+++ app/admin/products/[id]/page.tsx
@@ -3,7 +3,7 @@ import { getProductById } from '@/lib/actions/product.actions';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
-export const metada: Metadata = {
+export const metadata: Metadata = {
   title: 'Update Product',
 };
 
